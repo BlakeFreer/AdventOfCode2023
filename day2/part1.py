b94ab7d7e1e899c1f92915f60b0217fca582adb0
BlakeFreer <bkfreer10@gmail.com>
2024-02-05 23:36:47 -0500

    day2

---
+++ day2/part1.py
@@ -0,0 +1,41 @@
+"""
+Author: Blake Freer
+Date Created: 2024-02-05
+"""
+
+import logging
+import re
+import sys
+
+# logging.getLogger().setLevel("DEBUG")
+
+with open(sys.argv[1]) as f:
+    games = f.readlines()
+
+cube_count = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
+
+possible_games = 0
+
+pat_game_number = re.compile(r"Game ([\d]*):")
+
+pat_count = {col: re.compile(rf"([\d]*) {col}") for col in cube_count.keys()}
+
+for game in games:
+    game_number = int(pat_game_number.search(game).groups()[0])
+
+    max_count = {}
+    for col in cube_count.keys():
+        found = pat_count[col].findall(game)
+        logging.debug(found)
+        m = max(map(int, found))
+        logging.debug(m)
+        max_count[col] = m
+
+    if all([cube_count[i] >= max_count[i] for i in cube_count.keys()]):
+        possible_games += game_number
+
+print(possible_games)
